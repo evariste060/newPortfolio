5ceddfa664ab4d40a9067b771731a5b1773cd1af
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2025-10-03 08:14:45 +0200

    add dark mode

--- src/main.js
+++ src/main.js
@@ -3,25 +3,28 @@ let open = document.getElementById('open')
 let close =document.getElementById('close')
 let menu = document.getElementById('menu')
 open.addEventListener('click',()=>{
-    close.classList.toggle('hidden');
+    close.classList.toggle('hidden')
     menu.classList.toggle('hidden')
-
 })
 close.addEventListener('click',()=>{
-    close.classList.toggle('hidden');
-    menu.classList.toggle('hidden');
+    menu.classList.toggle('hidden')
 })
-let dark = document.getElementById('dark');
-let phoneDark = document.getElementById('phone')
+const phoneDark = document.getElementById('phone');
+const dark = document.getElementById('dark')
+// function darkMode(){
+//     document.querySelector(html).classList.toggle('dark')
+// }
+// dark.addEventListener('click',darkMode)
 function changeMode(){
     const element = document.querySelector('html').classList
     element.toggle('dark')
+
 }
 dark.addEventListener('click',changeMode)
-phoneDark.addEventListener('click',()=>{
+// phoneDark.addEventListener('click',changeMode)
+phoneDark.addEventListener('click',(e)=>{
+    e.preventDefault()
     const element = document.querySelector('html').classList
     element.toggle('dark')
   menu.classList.toggle('hidden');
 })
-console.log(phoneDark)
-console.log(dark)
